dd9b859760fb12255f0f01cd9f6f9f741eba77c1
Sergio Colado <sergio.colado@lightcurve.io>
2019-05-10 10:27:45 +0200

    Remove progress animation

--- src/components/send/progress.js
+++ src/components/send/progress.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Animated, Dimensions } from 'react-native';
+import { View, Dimensions } from 'react-native';
 import connect from 'redux-connect-decorator';
 import withTheme from '../withTheme';
 import getStyles from './styles';
@@ -8,27 +8,6 @@ import getStyles from './styles';
   activeToken: state.settings.token.active,
 }))
 class Progress extends React.Component {
-  state = {
-    progressRatio: new Animated.Value(0),
-  }
-
-  componentDidMount() {
-    this.setProgress(0);
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.current !== this.props.current) {
-      this.setProgress(300);
-    }
-  }
-
-  setProgress(duration) {
-    Animated.timing(this.state.progressRatio, {
-      toValue: this.props.current / (this.props.total - 1),
-      duration,
-    }).start();
-  }
-
   render() {
     const {
       styles, current, total,
